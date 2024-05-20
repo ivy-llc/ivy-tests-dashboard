bb7fe0de48b437248733bdd6c872b6d576c49efd
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-21 00:05:47 +0100

    initial workflow

---
+++ update_dashboard.py
@@ -0,0 +1,104 @@
+import argparse
+from collections import defaultdict
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Write the test results for the remote MongoDB to the dashboard")
+    parser.add_argument("--db-key", type=str, help="Key for the MongoDB database")
+
+    args = parser.parse_args()
+
+    uri = f"mongodb+srv://{args.db_key}@ivytestdashboard.mnzyom5.mongodb.net/?retryWrites=true&w=majority&appName=IvyTestDashboard"
+    client = MongoClient(uri)
+    db = client.ivytestdashboard
+    collection = db["test_results"]
+
+    records = collection.find()
+
+    missing_button = f"[![missing](https://img.shields.io/badge/missing-gray)]()"
+    test_results = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: {'numpy': missing_button, 'jax': missing_button, 'tensorflow': missing_button, 'torch': missing_button})))
+
+    color_codes = {
+        "passed": "green",
+        "failed": "red",
+        "skipped": "yellow",
+        "missing": "gray",
+    }
+
+    for record in records:
+        path = record['path']
+        
+        if path.startswith("test_frontends/test_jax/"):
+            path = path.replace("test_frontends/test_jax/", "")
+            base = "JAX Frontends"
+        elif path.startswith("test_frontends/test_numpy/"):
+            path = path.replace("test_frontends/test_numpy/", "")
+            base = "Numpy Frontends"
+        elif path.startswith("test_frontends/test_tensorflow/"):
+            path = path.replace("test_frontends/test_tensorflow/", "")
+            base = "TensorFlow Frontends"
+        elif path.startswith("test_frontends/test_torch/"):
+            path = path.replace("test_frontends/test_torch/", "")
+            base = "PyTorch Frontends"
+        elif path.startswith("test_functional/test_core/"):
+            path = path.replace("test_functional/test_core/", "")
+            base = "Ivy Functional API - Core"
+        elif path.startswith("test_functional/test_nn/"):
+            path = path.replace("test_functional/test_nn/", "")
+            base = "Ivy Functional API - NN"
+        elif path.startswith("test_functional/test_experimental/test_core/"):
+            path = path.replace("test_functional/test_experimental/test_core/", "")
+            base = "Ivy Functional API - Experimental Core"
+        elif path.startswith("test_functional/test_experimental/test_nn/"):
+            path = path.replace("test_functional/test_experimental/test_nn/", "")
+            base = "Ivy Functional API - Experimental NN"
+        else:
+            base = "Other"
+        
+        function = record['function']
+        backend = record['backend']
+        outcome = record['outcome']
+        workflow_link = record['workflow_link']
+        color = color_codes.get(outcome, 'yellow')
+        button = f"[![{outcome}](https://img.shields.io/badge/{outcome}-{color})]({workflow_link})"
+        test_results[base][path][function][backend] = button
+
+    # sort the paths & functions
+    sorted_paths = sorted(test_results.keys())
+    sorted_test_results = {path: dict(sorted(test_results[path].items())) for path in sorted_paths}
+
+    readme_content = "# Ivy Test Dashboard\n\n"
+
+    for base, path_functions in sorted_test_results.items():
+        readme_content += f"<div style='margin-top: 35px; margin-bottom: 20px; margin-left: 25px;'>\n"
+
+        # default the final drop-down to be open
+        if base == "TensorFlow Frontends":
+            readme_content += f"<details open>\n<summary><span style='font-size: 1.5em; font-weight: bold'>{base}</span></summary>\n\n"
+        else:
+            readme_content += f"<details>\n<summary style='margin-right: 10px;'><span style='font-size: 1.5em; font-weight: bold'>{base}</span></summary>\n\n"
+
+        for path, functions in path_functions.items():
+            readme_content += f"<div style='margin-top: 7px; margin-botton: 1px; margin-left: 25px;'>\n"
+
+            # default one the last test sets to be open
+            if base == "TensorFlow Frontends" and path == "test_tensor.py":
+                readme_content += f"<details open>\n<summary><span style=''>{path}</span></summary>\n\n"
+            else:
+                readme_content += f"<details>\n<summary><span style=''>{path}</span></summary>\n\n"
+
+            readme_content += "| Function | numpy | jax | tensorflow | torch |\n"
+            readme_content += "|----------|-------|-----|------------|-------|\n"
+
+            for function, results in functions.items():
+                readme_content += f"| {function} | {results['numpy']} | {results['jax']} | {results['tensorflow']} | {results['torch']} |\n"
+
+            readme_content += "</details>\n\n"
+            readme_content += "</div>\n\n"
+        readme_content += "</details>\n\n"
+        readme_content += "</div>\n\n"
+
+    # Write the content to README.md
+    with open("README.md", "w") as f:
+        f.write(readme_content)
