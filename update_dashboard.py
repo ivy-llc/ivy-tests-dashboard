822d0ee66c71d1e9a7f145a65524d11e55010cb9
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-22 01:32:00 +0100

    print new readme contents

--- update_dashboard.py
+++ update_dashboard.py
@@ -105,8 +105,10 @@ if __name__ == "__main__":
         readme_content += "</div>\n\n"
     readme_content += "\nThis dashboard is automatically updated nightly. If it hasn't been updated in within the last couple of days, feel free to raise an issue on the ivy repo."
 
-    print(readme_content)
-
-    # Write the content to README.md
     with open("README.md", "w") as f:
         f.write(readme_content)
+
+    with open("README.md", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line)
